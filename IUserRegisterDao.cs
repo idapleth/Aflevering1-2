4b147db63adeba6bfb7ea357c730b5e9f52fe3dd
idapleth <102719902+idapleth@users.noreply.github.com>
2023-10-25 13:22:10 +0200

    Update IUserRegisterDao.cs

--- IUserRegisterDao.cs
+++ IUserRegisterDao.cs
@@ -1,16 +1,1 @@
-using Domain;
 
-namespace Application;
-
-public interface IUserRegisterDao
-{
-    Task<UserRegister> CreateAsync(UserRegister user);
-    Task<UserRegister?> GetByFirstNameAsync(string firstName);
-    Task<UserRegister?> GetByLastNameAsync(string lastName);
-    Task<UserRegister?> GetByUserNameAsync(string userName);
-    Task<UserRegister?> GetByEmailAsync(string email);
-    Task<UserRegister?> GetByPasswordAsync(string password);
-    Task<UserRegister?> GetByIdAsync(int id);
-    public Task<IEnumerable<UserRegister>> GetAsync(SearchUserRegisterParametersDto searchRegisterParameters);
-    Task<UserRegister?> GetByUsernameAsync(string dtoUserName);
-}
