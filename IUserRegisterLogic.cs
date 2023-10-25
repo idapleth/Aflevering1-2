829ffdbcb1becd3d189ce90f3c025cd654f796f9
idapleth <102719902+idapleth@users.noreply.github.com>
2023-10-25 13:24:03 +0200

    Update IUserRegisterLogic.cs

--- IUserRegisterLogic.cs
+++ IUserRegisterLogic.cs
@@ -1,9 +1,1 @@
-using Domain;
 
-namespace Application;
-
-public interface IUserRegisterLogic
-{
-    public Task<UserRegister> CreateAsync(UserRegisterCreationDto dto);
-    public Task<IEnumerable<UserRegister>> GetAsync(SearchUserRegisterParametersDto searchRegisterParameters);
-}
